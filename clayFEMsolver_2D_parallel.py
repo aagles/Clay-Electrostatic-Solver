69ffa871affe055810d816abe8113f245762a0b7
Avery Agles <aagles@princeton.edu>
2023-08-09 12:14:11 -0700

    first pass

--- clayFEMsolver_2D_parallel.py
+++ clayFEMsolver_2D_parallel.py
@@ -28,7 +28,7 @@ from numba import jit, prange
 
 # Function to solve subdomains
 def solve_subdomain(args):
-    subdomain, psi_old, solid, rho0, T, a, b, c = args
+    subdomain, psi_old, solid, rho0, T, a, c = args
 
     psi_domain = psi_old.copy() #[subdomain[0]:subdomain[1],:,:]
     # Update the potential field
@@ -36,63 +36,52 @@ def solve_subdomain(args):
     x_end = subdomain[1]-1 if subdomain[1]==psi_old.shape[0] else subdomain[1] # want to take the penultimate index for last subdomain
 
     for i in range(x_start, x_end):
-        for j in range(1, subdomain[2] - 1):
-            for k in range(1, subdomain[3] - 1):
-                if solid[i, j, k] == 0: # Only update for fluid points
+            for k in range(1, subdomain[2] - 1):
+                if solid[i, k] == 0: # Only update for fluid points
 
                     RHS = (((rho0*constants.e)/(eps*constants.epsilon_0)) 
-                            * (np.exp(-constants.e*psi_old[i,j,k]/(constants.k*T)) - np.exp(constants.e*psi_old[i,j,k]/(constants.k*T))))
+                            * (np.exp(-constants.e*psi_old[i,k]/(constants.k*T)) - np.exp(constants.e*psi_old[i,j,k]/(constants.k*T))))
                     
                     # Solving for Psi[i,j,k]
-                    # for eqn with the form: (2x+A)/a^2 + (2x+B)/b^2 + (2x+C)/c^2 = D
-                    # x = [Da^2b^2c^2 - Ab^2c^2 - Ba^2c^2 - Ca^2b^2] / 2(b^2c^2 + a^2c^2 + a^2b^2)
-                    A = psi_old[i+1,j,k] + psi_old[i-1,j,k]
-                    B = psi_old[i,j+1,k] + psi_old[i,j-1,k]
-                    C = psi_old[i,j,k+1] + psi_old[i,j,k-1]
+                    # for eqn with the form: (2x+A)/a^2 + (2x+C)/c^2 = D
+                    # x = [Da^2b^2c^2 - Ac^2 - Ca^2] / 2(c^2 + a^2c^2 + a^2)
+                    A = psi_old[i+1,k] + psi_old[i-1,k]
+                    C = psi_old[i,k+1] + psi_old[i,k-1]
                     D = RHS 
-                    numerator = (D * a**2 * b**2 * c**2) - (A * b**2 * c**2 + B * a**2 * c**2 + C * a**2 * b**2)
-                    denominator = 2 * (b**2 * c**2 + a**2 * c**2 + a**2 * b**2)
+                    numerator = (D * a**2 * c**2) - (A * c**2 +  C * a**2)
+                    denominator = 2 * (c**2 + a**2 * c**2 + a**2)
 
-                    psi_domain[i, j, k] = numerator / denominator
+                    psi_domain[i, k] = numerator / denominator
 
-    # Apply Neumann boundary conditions at the edges of the domain
-    # psi_new[0, :, :] = psi_new[1, :, :]
-    # psi_new[-1, :, :] = psi_new[-2, :, :]
-    # psi_new[:, 0, :] = psi_new[:, 1, :]
-    # psi_new[:, -1, :] = psi_new[:, -2, :]
-    # psi_new[:, :, 0] = psi_new[:, :, 1]
-    # psi_new[:, :, -1] = psi_new[:, :, -2]
 
-    return psi_domain[x_start:x_end,:,:]
+    return psi_domain[x_start:x_end,:]
 
 
 # ## Initialize Background Mesh (Class), $\Omega$
 # Create a class to represent the total mesh and to define the functions that need to be solved everywhere in the system
 #
 class Omega:
-    def __init__(self, Lx, Ly, Lz, dx, dy, dz):
+    def __init__(self, Lx, Lz, dx, dy, dz):
         # size of the mesh in units of nm
         self.Lx = Lx    # Size of the mesh in the x-direction
-        self.Ly = Ly    # Size of the mesh in the y-direction
         self.Lz = Lz    # Size of the mesh in the z-direction
         self.dx = dx    # Dimension of the mesh elements in the x-direction, also in nm
-        self.dy = dy    # Dimension of the mesh elements in the y-direction
+        # self.dy = dy    # Dimension of the mesh elements in the y-direction
         self.dz = dz    # Dimension of the mesh elements in the z-direction
 
 
         # Calculate the number of elements in each direction
         self.Nx = int(self.Lx / self.dx)
-        self.Ny = int(self.Ly / self.dy)
         self.Nz = int(self.Lz / self.dz)
 
         
         # Initialize Variables for the SOLID mesh elements with numpy zeros
 
-        self.solid = np.zeros((self.Nx, self.Ny, self.Nz)) # 1 where solid element, zero everywhere else
-        self.surf = np.zeros((self.Nx, self.Ny, self.Nz))  # 1 where surf element, zero everywhere else
-        self.sigma = np.zeros((self.Nx, self.Ny, self.Nz)) # sigma_value where solid, zero everywhere else
-        self.psi = np.zeros((self.Nx, self.Ny, self.Nz))
-        self.force = np.zeros((self.Nx, self.Ny, self.Nz))
+        self.solid = np.zeros((self.Nx, self.Nz)) # 1 where solid element, zero everywhere else
+        self.surf = np.zeros((self.Nx, self.Nz))  # 1 where surf element, zero everywhere else
+        self.sigma = np.zeros((self.Nx, self.Nz)) # sigma_value where solid, zero everywhere else
+        self.psi = np.zeros((self.Nx, self.Nz))
+        self.force = np.zeros((self.Nx, self.Nz))
 
         # Initialize Variables for the FLUID mesh elements with numpy zeros
         # self.rho_p = np.zeros((self.Nx, self.Ny, self.Nz))
@@ -104,10 +93,10 @@ class Omega:
 
     ### Properties Methods
     def get_dimensions(self):
-        return self.Lx, self.Ly, self.Lz, self.dx, self.dy, self.dz
+        return self.Lx, self.Lz, self.dx, self.dy, self.dz
 
     def get_num_elements(self):
-        return self.Nx, self.Ny, self.Nz
+        return self.Nx, self.Nz
 
     
     ####################### INITIALIZATION ###############################################################
@@ -115,7 +104,7 @@ class Omega:
     ## SOLID Initialization Methods
     #### Initialize the indices for a solid element
 
-    def initialize_solid(self, Sx, Sy, d, R, loc, oneD, sigma_value, read, dir):
+    def initialize_solid(self, Sx, Sy, d, R, loc, sigma_value, read, dir):
         """
         Defining the self.solid and self.surf for a solid rectangular object with width Sx by Sy and thickness, d
         Centering the object at [x_pos, y_pos, z_pos].
@@ -131,37 +120,34 @@ class Omega:
             self.sigma = np.load(os.path.join(dir,'sigma.npy'))
         else:
             half_Sx = Sx / 2
-            half_Sy = Sy / 2
             half_d  = d  / 2
 
             for i in range(self.Nx):
-                for j in range(self.Ny):
-                    for k in range(self.Nz):
-                        x = i * self.dx # x-coordinate
-                        y = j * self.dy # y-coordinate
-                        z = k * self.dz # z-coordinate
+                for k in range(self.Nz):
+                    x = i * self.dx # x-coordinate
+                    z = k * self.dz # z-coordinate
 
-                        if abs(x - loc[0]) <= half_Sx and abs(y - loc[1]) <= half_Sy:
-                            z_squared = R**2 - (x - loc[0])**2   # - (y - y_pos)**2
+                    if abs(x - loc[0]) <= half_Sx:
+                        z_squared = R**2 - (x - loc[0])**2   
 
-                            if z_squared >= 0:
-                                z = int(np.sqrt(z_squared) + loc[2] - R)
-                                start_index_z = int( (z - half_d) / self.dz )       
-                                end_index_z = int( (z + half_d) / self.dz )
+                        if z_squared >= 0:
+                            z = int(np.sqrt(z_squared) + loc[2] - R)
+                            start_index_z = int( (z - half_d) / self.dz )       
+                            end_index_z = int( (z + half_d) / self.dz )
 
-                                start_index_z = max(0, start_index_z)
-                                end_index_z = min(self.Nz - 1, end_index_z)
+                            start_index_z = max(0, start_index_z)
+                            end_index_z = min(self.Nz - 1, end_index_z)
 
-                                # Label Solid elements
-                                for kk in range(start_index_z, end_index_z + 1):
-                                    self.solid[i, j, kk] = 1
+                            # Label Solid elements
+                            for kk in range(start_index_z, end_index_z + 1):
+                                self.solid[i, kk] = 1
 
-                                # Label Surface Elements
-                                self.surf[i, j, start_index_z] = 1
-                                self.surf[i, j, end_index_z] = 1
+                            # Label Surface Elements
+                            self.surf[i, start_index_z] = 1
+                            self.surf[i, end_index_z] = 1
 
-                                self.sigma[i, j, start_index_z] = sigma_value
-                                self.sigma[i, j, end_index_z] = sigma_value
+                            self.sigma[i, start_index_z] = sigma_value
+                            self.sigma[i, end_index_z] = sigma_value
 
     def save_mesh(self, dir):
         # save all the the necessary files as numpy arrays in the inputted dir
@@ -174,23 +160,16 @@ class Omega:
     # Plotting the Object
     def plot_object(self):
         fig = plt.figure(figsize=(8, 6))
-        ax = fig.add_subplot(111, projection='3d')
+        ax = fig.add_subplot(111)
 
-        solid_indices = np.where(self.solid == 1)  # Get the indices where solid exists
-
-        ax.scatter(solid_indices[0], solid_indices[1], solid_indices[2], c='b', marker='o')
-
-        ax.set_xlim([0,self.Lx])
-        ax.set_ylim([0,self.Ly])
-        ax.set_zlim([0,self.Lz])
-
-        ax.set_xlabel('X')
-        ax.set_ylabel('Y')
-        ax.set_zlabel('Z')
-        plt.title('3D Representation of Solid Object')
-
+        plt.imshow(self.solid.T, origin='lower', extent=[0, self.Lx, 0, self.Lz], cmap='viridis') #, vmin=-0.2, vmax=0.8)
+        plt.colorbar(label='Potential $\Psi (mV)$')
+        plt.xlabel('x (nm)')
+        plt.ylabel('z (nm)')
+        plt.title(f'Shape of 2D object in xz plane')
+        plt.savefig('../data/2d_solid.png', dpi=300)
         plt.show()
-    
+
 
 
 
@@ -226,7 +205,7 @@ class Omega:
         print(psi_s)
 
         # Subdomain Information
-        subdomains = [(i*self.Nx//num_processes, (i+1)*self.Nx//num_processes, self.Ny, self.Nz) for i in range(num_processes)]
+        subdomains = [(i*self.Nx//num_processes, (i+1)*self.Nx//num_processes, self.Nz) for i in range(num_processes)]
 
         # Define tolerance and maximum iterations for convergence
         tolerance = 1e-6
@@ -236,19 +215,18 @@ class Omega:
         for iteration in range(max_iterations):
             if (iteration % 10) == 0:
                 print('Iteration '+str(iteration))
-                np.save('../data/current_psi_'+name+'.npy', self.psi)
+                np.save('../data/2D_current_psi'+name+'.npy', self.psi)
             elif (iteration == max_iterations-1):
-                np.save('../data/psi_map_'+name+'.npy', self.psi)
+                np.save('../data/2D_psi_map'+name+'.npy', self.psi)
 
             # Create a copy of the potential field to calculate the change
             psi_old = self.psi.copy()
             
             # call the parallel calculation of the entire domain
-            #args = (lower_x, upper_x, Ny, Nz, psi_old, solid, rho0, T, a, b, c)
+            #args = (lower_x, upper_x, Nz, psi_old, solid, rho0, T, a, b, c)
             a = self.dx * (1e-9) # converting to units of m
-            b = self.dy * (1e-9)
             c = self.dz * (1e-9)
-            args = [(subdomain, psi_old, self.solid, rho0, T, a, b, c) for subdomain in subdomains]
+            args = [(subdomain, psi_old, self.solid, rho0, T, a, c) for subdomain in subdomains]
             
             with Pool(num_processes) as p:
                 results = p.map(solve_subdomain, args)
@@ -259,25 +237,23 @@ class Omega:
                 if subdomains[i][0]==0: # if first subdomain
                     x_start = subdomains[i][0]+1
                     x_end = subdomains[i][1]
-                    self.psi[x_start:x_end, :, :]  = result
+                    self.psi[x_start:x_end, :]  = result
                 elif subdomains[i][1]==psi_old.shape[0]:# if the last subdomain
                     x_start = subdomains[i][0]
                     x_end = subdomains[i][1]-1  
-                    self.psi[x_start:x_end, :, :] = result
+                    self.psi[x_start:x_end, :] = result
                 else: 
                     x_start = subdomains[i][0]
                     x_end = subdomains[i][1]
-                    self.psi[x_start:x_end, :, :] = result
+                    self.psi[x_start:x_end, :] = result
 
             
                 
                 # Apply Neumann boundary conditions at the edges of the domain
-                self.psi[0, :, :] = self.psi[1, :, :]
-                self.psi[-1, :, :] = self.psi[-2, :, :]
-                self.psi[:, 0, :] = self.psi[:, 1, :]
-                self.psi[:, -1, :] = self.psi[:, -2, :]
-                self.psi[:, :, 0] = self.psi[:, :, 1]
-                self.psi[:, :, -1] = self.psi[:, :, -2]
+                self.psi[0, :] = self.psi[1, :]
+                self.psi[-1, :] = self.psi[-2, :]
+                self.psi[:, 0] = self.psi[:, 1]
+                self.psi[:, -1] = self.psi[:, -2]
 
             # Check for convergence
             # if np.max(np.abs(psi_old - self.psi)) < tolerance:
@@ -292,7 +268,7 @@ class Omega:
         if iteration == max_iterations - 1:
             print('Warning: solve_laplace did not converge')
 
-        np.save('../data/abs_error_'+name+'.npy', abs_error)
+        np.save('../data/2D_abs_error_'+name+'.npy', abs_error)
 
 
     # Method to save the Psi map
@@ -300,18 +276,12 @@ class Omega:
         np.save(fname, self.psi)
 
     # Method to plot Psi
-    def plot_psi(self, y_pos, read, fn):
-        # Check if y_index is within the domain
-        if y_pos < 0 or y_pos >= self.Ly:
-            print(f'Error: y_index should be in the range [0, {self.Ly}]')
-            return
-
+    def plot_psi(self, read, fn):
         # Get the slice of the potential field at the given y-index
-        y_index = int(round(y_pos/self.dy))
         if read:
-            psi_slice = np.load(fn)[:, y_index, :]
+            psi_slice = np.load(fn)[:, :]
         else:
-            psi_slice = self.psi[:, y_index, :]
+            psi_slice = self.psi[:, :]
         
         psi_slice = psi_slice * 1000 # converting to mV
 
@@ -321,8 +291,8 @@ class Omega:
         plt.colorbar(label='Potential $\Psi (mV)$')
         plt.xlabel('x (nm)')
         plt.ylabel('z (nm)')
-        plt.title(f'Density map of potential $\Psi (mV)$ in xz plane at y-index {y_index}')
-        plt.savefig('../data/psi_final.png', dpi=300)
+        plt.title(f'Density map of potential $\Psi (mV)$ in xz plane')
+        plt.savefig('../data/2D_psi_final.png', dpi=300)
         plt.show()
         
 
@@ -341,17 +311,16 @@ rho0        = rho0_M * constants.Avogadro * 1000  # rho density in units of ions
 T           = 300  # temperature (K)
 name        = 'rho0_1'
 
-# Steps to implement:
-oneD=False
+
 
 if __name__ == '__main__':
     freeze_support()
-    omega = Omega(Lx=20, Ly=20, Lz=20, dx=.1, dy=.1, dz=.1)
-    omega.initialize_solid(Sx=10, Sy=10, d=.2, R=8, loc=[10,10,10], oneD=oneD, sigma_value=sigma_value, read=True, dir='../mesh/')
+    omega = Omega(Lx=20, Lz=20, dx=.1, dz=.1)
+    omega.initialize_solid(Sx=10, d=.2, R=8, loc=[10,10], sigma_value=sigma_value, read=True, dir='../mesh/')
     # omega.save_mesh('/Volumes/GoogleDrive/My Drive/research/projects/LBNL/ClayFEMsolver/')
     # omega.plot_object()
-    omega.solve_fluid_parallel(rho0=rho0, T=T, num_processes=8)
-    omega.save_psi('../data/psi_map_'+name+'.npy')
+    # omega.solve_fluid_parallel(rho0=rho0, T=T, num_processes=8)
+    # omega.save_psi('../data/psi_map_'+name+'.npy')
     # omega.plot_psi(y_pos=10, read=True, fn='../data/final_psi_rho0_'+str(rho0_M)+'.npy')
 
 
